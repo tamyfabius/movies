c3cd2668c06f09c37d4124d1a1970f9da6d27ab0
t.de-giova <tamyfabius@gmail.com>
2017-06-09 12:04:33 +0200

    création d'un constant PORT et update app.js en ES6

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // importer le module express
 const express = require('express');
-
 // créer l'application
 const app = express();
 
-/*
+const PORT = 3000;
 
- */
-app.get('/', function(req, res) {
-    res.send('Hello world');
-});
 
-app.listen(3000, ()=> console.log('on est sur le port 3000'));
+app.get('/', (req, res) => { res.send('Hello world') });
+
+app.listen(PORT, () => { console.log(`on est sur le port : ${PORT}`) });
+
+
