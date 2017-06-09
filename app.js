d4a5faffc368f4dfc22eedeaaa38730ceb243cbf
t.de-giova <tamyfabius@gmail.com>
2017-06-09 17:38:48 +0200

    Passer des données complexs à une vue et itérer côté template

--- app.js
+++ app.js
@@ -13,26 +13,26 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/movies', (req, res) => {
-    //res.send('Bientôt des films ici même');
-    res.render('movies');
-});
 
-// app.get('/movies-details', (req, res) => {
-//     res.render('movies-details');
-// });
+    const title = 'Films français des trentes dernières années';
+
+    const frenchMovies = [
+        { title: 'Le fabuleux destin d\'Amélie Poulain', year: 2001},
+        { title: 'Buffet froid', year: 1979},
+        { title: 'Le diner de cons', year: 1998},
+        { title: 'De rouille et d\'os', year: 2012},
+    ];
+    res.render('movies', { movies: frenchMovies, title: title});
+});
 
 app.get('/movies/add', (req, res) => {res.send('formulaire')});
 
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    // const title = 'Termnator';
-    // const title = req.params.title;
-
     res.render('movies-details', {movieId: id});
 });
 
 app.get('/', (req, res) => {
-    //res.send('Hello world');
     res.render('index');
 });
 
