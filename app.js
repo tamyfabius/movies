ce4b36236327aa32fd26c140bfadfeec1bdc93be
t.de-giova <tamyfabius@gmail.com>
2017-06-09 16:31:13 +0200

    ajout de css, ajout du middleware, ajout de la views, update d'autre fichier comm app.js.

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const app = express();
 const PORT = 3000;
 
 
+/* MIDDLEWARE */
+app.use('/public', express.static('public'));
+
+
 /* ROUTES */
+app.set('views', './views');
+app.set('view engine', 'ejs');
 app.get('/movies', (req, res) => {res.send('Bientôt des films ici même')});
 app.get('/movies/add', (req, res) => {res.send('formulaire')});
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
     res.send(`Movies numéro ${id}`);
 });
-
-
-
-app.get('/', (req, res) => { res.send('Hello world') });
+app.get('/', (req, res) => {
+    //res.send('Hello world');
+    res.render('index');
+});
 
 app.listen(PORT, () => { console.log(`on est sur le port : ${PORT}`) });
 
