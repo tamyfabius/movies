18d470096a7739f180b1d9664352d253a822bea7
t.de-giova <tamyfabius@gmail.com>
2017-06-09 15:45:54 +0200

    update app.js : importance des routes

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const app = express();
 const PORT = 3000;
 
 
+/* ROUTES */
+app.get('/movies', (req, res) => {res.send('Bientôt des films ici même')});
+app.get('/movies/add', (req, res) => {res.send('formulaire')});
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Movies numéro ${id}`);
+});
+
+
+
 app.get('/', (req, res) => { res.send('Hello world') });
 
 app.listen(PORT, () => { console.log(`on est sur le port : ${PORT}`) });
