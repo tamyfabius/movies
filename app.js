b85d31da53b1e1433419da01fa8f8e5b3e613b9c
t.de-giova <tamyfabius@gmail.com>
2017-06-09 16:56:00 +0200

    Pratiquer l'utilisation des templates

--- app.js
+++ app.js
@@ -13,11 +13,18 @@ app.use('/public', express.static('public'));
 /* ROUTES */
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.get('/movies', (req, res) => {res.send('Bientôt des films ici même')});
+app.get('/movies', (req, res) => {
+    //res.send('Bientôt des films ici même');
+    res.render('movies');
+});
+// app.get('/movies-details', (req, res) => {
+//     res.render('movies-details');
+// });
 app.get('/movies/add', (req, res) => {res.send('formulaire')});
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    res.send(`Movies numéro ${id}`);
+    //res.send(`Movies numéro ${id}`);
+    res.render('movies-details');
 });
 app.get('/', (req, res) => {
     //res.send('Hello world');
