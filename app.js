6c80e32d3f86dc23d67bf45b1ae09c58ef449e72
t.de-giova <tamyfabius@gmail.com>
2017-06-09 17:09:06 +0200

    Passer des données à une vue

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// importer le module express
 const express = require('express');
-// créer l'application
 const app = express();
-
 const PORT = 3000;
 
 
@@ -12,20 +9,28 @@ app.use('/public', express.static('public'));
 
 /* ROUTES */
 app.set('views', './views');
+
 app.set('view engine', 'ejs');
+
 app.get('/movies', (req, res) => {
     //res.send('Bientôt des films ici même');
     res.render('movies');
 });
+
 // app.get('/movies-details', (req, res) => {
 //     res.render('movies-details');
 // });
+
 app.get('/movies/add', (req, res) => {res.send('formulaire')});
+
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    //res.send(`Movies numéro ${id}`);
-    res.render('movies-details');
+    // const title = 'Termnator';
+    // const title = req.params.title;
+
+    res.render('movies-details', {movieId: id});
 });
+
 app.get('/', (req, res) => {
     //res.send('Hello world');
     res.render('index');
